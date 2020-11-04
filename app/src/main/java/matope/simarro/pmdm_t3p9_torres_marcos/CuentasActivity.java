5936cab7f13e665dcb99b5314c686b5d1426dbc9
marcostorresperez <torresperezmarcos92@gmail.com>
2020-11-04 11:46:30 +0100

    4 NOV - falta añadir movimientos 2

--- app/src/main/java/matope/simarro/pmdm_t3p9_torres_marcos/CuentasActivity.java
+++ app/src/main/java/matope/simarro/pmdm_t3p9_torres_marcos/CuentasActivity.java
@@ -21,11 +21,13 @@ import matope.simarro.pmdm_t3p9_torres_marcos.bd.MiBancoOperacional;
 import matope.simarro.pmdm_t3p9_torres_marcos.dao.CuentaDAO;
 import matope.simarro.pmdm_t3p9_torres_marcos.pojo.Cliente;
 import matope.simarro.pmdm_t3p9_torres_marcos.pojo.Cuenta;
+import matope.simarro.pmdm_t3p9_torres_marcos.pojo.Movimiento;
 
 public class CuentasActivity extends AppCompatActivity {
     private ListView origen;
     private TextView seleccionado = null;
     CuentasAdapter<Cuenta> cuentas;
+    MovimientosAdapter<Movimiento> movimientos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +47,10 @@ public class CuentasActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Cliente cliente = (Cliente) getIntent().getSerializableExtra("cliente");
+                MiBancoOperacional api = MiBancoOperacional.getInstance(getApplicationContext());
 
+                movimientos = new MovimientosAdapter(this,api.getMovimientos(this.onItemClick(););
             }
         });
 
